500c4c0693ffea31d6feab89bea0e4d41275a3c8
Fede <delRincon@gmail.com>
2021-04-24 12:19:15 -0300

    370- imports and routes

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 // Definir puerto
 const port = process.env.PORT || 4000;
 
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+app.get('/nosotros', (req, res) => {
+    res.send('Nosotros');
+});
+
+app.get('/contacto', (req, res) => {
+    res.send('Contacto');
+});
 
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`)

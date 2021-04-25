cbfed6f3f41460a3f018d3f2465cb7f3b69fe396
Fede <delRincon@gmail.com>
2021-04-24 22:43:46 -0300

    377- Footer + middleware of year

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ const port = process.env.PORT || 4000;
 //Habilitar PUG (equivalente a HBS o EJS) (HBS seria lo mas parecido a react)
 app.set('view engine', 'pug');
 
+//obtener el año actual
+app.use( (req, res, next) => {
+    const year = new Date();
+    res.locals.actualYear =year.getFullYear();     
+    next();
+})
+
 //Definir carpeta publica
 app.use(express.static('public'));
 
 //agregar Router
 app.use('/', router);
-
 
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`)

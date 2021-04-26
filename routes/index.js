d83e26ae55f106c0ab6b9318cc08de82d6657963
Fede <delRincon@gmail.com>
2021-04-26 19:29:00 -0300

    383- consultar DB

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { paginaInicio, paginaNosotros, paginaViajes, paginaTestimoniales } from '../controllers/paginasController.js';
+import { 
+    paginaInicio, 
+    paginaNosotros, 
+    paginaViajes, 
+    paginaTestimoniales
+} from '../controllers/paginasController.js';
 
 const router = express.Router();
 
